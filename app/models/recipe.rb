fa49aaa592e5b67d24d3a3daa72b697b11c376ba
Kubsonix <kuba.sniec@gmail.com>
2016-01-12 12:28:33 +0100

    Add comments

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,10 @@
 class Recipe < ActiveRecord::Base
 
    belongs_to :user
-   
+
    has_many :ingredients
    has_many :directions
+   has_many :comments
 
    accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
    accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['step'].blank? }, allow_destroy: true
