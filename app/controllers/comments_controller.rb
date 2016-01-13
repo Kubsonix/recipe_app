b6d66cbce0807cd36ba34b22ab9607bc82781f9a
Kubsonix <kuba.sniec@gmail.com>
2016-01-13 01:25:18 +0100

    Add name to users & change view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :edit, :destroy]
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112110035) do
+ActiveRecord::Schema.define(version: 20160112232330) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "recipe_id"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20160112110035) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
